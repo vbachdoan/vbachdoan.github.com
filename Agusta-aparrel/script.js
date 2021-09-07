81cbedfeca0e443b23b46dbfd15057544ce4d4af
vbachdoan <vbachdoan@gmail.com>
2021-09-08 04:39:02 +0700

    complete

--- Agusta-aparrel/script.js
+++ Agusta-aparrel/script.js
@@ -4,17 +4,6 @@
  * Slide 1's animation
  */
 
-// explore this comment 
-// let slide1_block1_image1, slide1_block1_image2,
-//     slide1_block3_image1, slide1_block3_image2,
-//     slide1_block4_image1, slide1_block4_image2,
-//     slide1_block4_image3,
-//     slide1_block5_image1, slide1_block5_image2,
-//     slide1_block7_image1, slide1_block7_image2
-// if (window.innerWidth >= 800) {
-//     slide1_block1_image1 = './image/slide1_block1_image1_biggest.png'
-// }
-
 document.getElementById('slide1_block1_logo1').addEventListener('mouseenter', () => {
     document.getElementById('slide1_block1_image').setAttribute('src', './image/slide1_block1_image1.png')
     document.getElementById('slide1_block1_logo1').style.border = '1px solid rgba(0,0,0,1)'
@@ -73,4 +62,9 @@ document.getElementById('slide1_block7_logo2').addEventListener('mouseenter', ()
     document.getElementById('slide1_block7_logo1').style.border = '1px solid rgba(0,0,0,0)'
     document.getElementById('slide1_block7_logo2').style.border = '1px solid rgba(0,0,0,1)'
 })
+
+/**
+ * Responsive
+ */
+
 
