4bf5e6df84a0e1cfedfe81c42c3b5d6271f4ab70
vbachdoan <vbachdoan@gmail.com>
2021-09-23 11:31:27 +0700

    js

--- react-agusta-dragster/src/components/Slide2.js
+++ react-agusta-dragster/src/components/Slide2.js
@@ -8,114 +8,114 @@ const Slide2 = () => {
         <div id="slide2">
             <div id="wrap_card">
                 {/* <!-- card 1 --> */}
-                <div class="card">
-                    <h2 class="card_name">ROSSO</h2>
+                <div className="card">
+                    <h2 className="card_name">ROSSO</h2>
                     <img src="./Image/slide2_image1.webp" alt="ROSSO model" loading="lazy" />
                     {/* <!-- providing information for card --> */}
-                    <div class="wrap_information">
-                        <div class="block_information">
-                            <div class="number"><h2>3</h2></div>
-                            <p class="information">Cylinders</p>
+                    <div className="wrap_information">
+                        <div className="block_information">
+                            <div className="number"><h2>3</h2></div>
+                            <p className="information">Cylinders</p>
                         </div>
-                        <div class="block_information">
-                            <div class="number">
+                        <div className="block_information">
+                            <div className="number">
                                 <h2>798</h2>
-                                <h3 class="sub_number">cc</h3>
+                                <h3 className="sub_number">cc</h3>
                             </div>
-                            <p class="information">Capacity</p>
+                            <p className="information">Capacity</p>
                         </div>
-                        <div class="block_information">
-                            <div class="number">
+                        <div className="block_information">
+                            <div className="number">
                                 <h2>110</h2>
-                                <h3 class="sub_number">hp</h3>
+                                <h3 className="sub_number">hp</h3>
                             </div>
-                            <p class="information">Horsepower</p>
+                            <p className="information">Horsepower</p>
                         </div>
                     </div>
-                    <div class="card_btn">LEARN MORE</div>
+                    <div className="card_btn">LEARN MORE</div>
                 </div>
 
                 {/* <!-- card 2 --> */}
-                <div class="card">
-                    <h2 class="card_name">RR</h2>
+                <div className="card">
+                    <h2 className="card_name">RR</h2>
                     <img src="./Image/slide2_image2.webp" alt="RR model" loading="lazy" />
                     {/* <!-- providing information for card --> */}
-                    <div class="wrap_information">
-                        <div class="block_information">
-                            <div class="number"><h2>3</h2></div>
-                            <p class="information">Cylinders</p>
+                    <div className="wrap_information">
+                        <div className="block_information">
+                            <div className="number"><h2>3</h2></div>
+                            <p className="information">Cylinders</p>
                         </div>
-                        <div class="block_information">
-                            <div class="number">
+                        <div className="block_information">
+                            <div className="number">
                                 <h2>798</h2>
-                                <h3 class="sub_number">cc</h3>
+                                <h3 className="sub_number">cc</h3>
                             </div>
-                            <p class="information">Capacity</p>
+                            <p className="information">Capacity</p>
                         </div>
-                        <div class="block_information">
-                            <div class="number">
+                        <div className="block_information">
+                            <div className="number">
                                 <h2>140</h2>
-                                <h3 class="sub_number">hp</h3>
+                                <h3 className="sub_number">hp</h3>
                             </div>
-                            <p class="information">Horsepower</p>
+                            <p className="information">Horsepower</p>
                         </div>
                     </div>
-                    <div class="card_btn">LEARN MORE</div>
+                    <div className="card_btn">LEARN MORE</div>
                 </div>
 
                 {/* <!-- card 3 --> */}
-                <div class="card">
-                    <h2 class="card_name">RR SCS</h2>
+                <div className="card">
+                    <h2 className="card_name">RR SCS</h2>
                     <img src="./Image/slide2_image3.webp" alt="RR SCS model" loading="lazy" />
                     {/* <!-- providing information for card --> */}
-                    <div class="wrap_information">
-                        <div class="block_information">
-                            <div class="number"><h2>3</h2></div>
-                            <p class="information">Cylinders</p>
+                    <div className="wrap_information">
+                        <div className="block_information">
+                            <div className="number"><h2>3</h2></div>
+                            <p className="information">Cylinders</p>
                         </div>
-                        <div class="block_information">
-                            <div class="number">
+                        <div className="block_information">
+                            <div className="number">
                                 <h2>798</h2>
-                                <h3 class="sub_number">cc</h3>
+                                <h3 className="sub_number">cc</h3>
                             </div>
-                            <p class="information">Capacity</p>
+                            <p className="information">Capacity</p>
                         </div>
-                        <div class="block_information">
-                            <div class="number">
+                        <div className="block_information">
+                            <div className="number">
                                 <h2>140</h2>
-                                <h3 class="sub_number">hp</h3>
+                                <h3 className="sub_number">hp</h3>
                             </div>
-                            <p class="information">Horsepower</p>
+                            <p className="information">Horsepower</p>
                         </div>
                     </div>
-                    <div class="card_btn">LEARN MORE</div>
+                    <div className="card_btn">LEARN MORE</div>
                 </div>
 
                 {/* card 4 */}
-                <div class="card">
-                    <h2 class="card_name">RC SCS</h2>
+                <div className="card">
+                    <h2 className="card_name">RC SCS</h2>
                     <img src="./Image/slide2_image4.webp" alt="RC SCS model" loading="lazy" />
-                    <div class="wrap_information">
-                        <div class="block_information">
-                            <div class="number"><h2>3</h2></div>
-                            <p class="information">Cylinders</p>
+                    <div className="wrap_information">
+                        <div className="block_information">
+                            <div className="number"><h2>3</h2></div>
+                            <p className="information">Cylinders</p>
                         </div>
-                        <div class="block_information">
-                            <div class="number">
+                        <div className="block_information">
+                            <div className="number">
                                 <h2>798</h2>
-                                <h3 class="sub_number">cc</h3>
+                                <h3 className="sub_number">cc</h3>
                             </div>
-                            <p class="information">Capacity</p>
+                            <p className="information">Capacity</p>
                         </div>
-                        <div class="block_information">
-                            <div class="number">
+                        <div className="block_information">
+                            <div className="number">
                                 <h2>150</h2>
-                                <h3 class="sub_number">hp</h3>
+                                <h3 className="sub_number">hp</h3>
                             </div>
-                            <p class="information">Horsepower</p>
+                            <p className="information">Horsepower</p>
                         </div>
                     </div>
-                    <div class="card_btn">LEARN MORE</div>
+                    <div className="card_btn">LEARN MORE</div>
                 </div>
 
             </div>
--- react-agusta-dragster/src/components/Slide2Card.js
+++ react-agusta-dragster/src/components/Slide2Card.js
@@ -2,31 +2,31 @@ import React from 'react'
 
 const Slide2Card = () => {
     return (
-        <div class="card">
-            <h2 class="card_name">RR</h2>
+        <div className="card">
+            <h2 className="card_name">RR</h2>
             <img src="./Image/slide2_image2.webp" alt="RR model" loading="lazy" />
             {/* <!-- providing information for card --> */}
-            <div class="wrap_information">
-                <div class="block_information">
-                    <div class="number"><h2>3</h2></div>
-                    <p class="information">Cylinders</p>
+            <div className="wrap_information">
+                <div className="block_information">
+                    <div className="number"><h2>3</h2></div>
+                    <p className="information">Cylinders</p>
                 </div>
-                <div class="block_information">
-                    <div class="number">
+                <div className="block_information">
+                    <div className="number">
                         <h2>798</h2>
-                        <h3 class="sub_number">cc</h3>
+                        <h3 className="sub_number">cc</h3>
                     </div>
-                    <p class="information">Capacity</p>
+                    <p className="information">Capacity</p>
                 </div>
-                <div class="block_information">
-                    <div class="number">
+                <div className="block_information">
+                    <div className="number">
                         <h2>140</h2>
-                        <h3 class="sub_number">hp</h3>
+                        <h3 className="sub_number">hp</h3>
                     </div>
-                    <p class="information">Horsepower</p>
+                    <p className="information">Horsepower</p>
                 </div>
             </div>
-            <div class="card_btn">LEARN MORE</div>
+            <div className="card_btn">LEARN MORE</div>
         </div>
     )
 }
